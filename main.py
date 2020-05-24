af87c8b239676652109e8967cc3728abdefd9c3f
SAVIN DMITRIY <savindmitriy@MacBook-Pro-SAVIN-2.local>
2020-05-24 21:33:43 +0300

    bot_1

--- main.py
+++ main.py
@@ -1,20 +1,83 @@
 import requests
 import configparser
 import logging
+import datetime
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 logging.basicConfig(format='%(pathname)s  %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 PATH = 'https://api.telegram.org/bot'
 
-def get_token():
-    token = config['DEFAULT']['token']
-    return token
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+now = datetime.datetime.now()
+token = config['DEFAILT']['token']
+
+
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
+
+
+def main():
+    greet_bot = BotHandler(token)
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
 
 
 if __name__ == '__main__':
-    logging.info('Starting')
-    token = get_token()
-
-
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
 
